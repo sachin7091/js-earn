af02005a0692c464a8f83797024c05ea485720f8
sachin7091 <sachinkumar37958@gmail.com>
2025-07-11 17:10:39 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3
+    +
+    3) //code readabilty should be high
+
+    console.log("sachin")
+
+    let name ="sachin"
+    let age = 18
+    let isLoggedIN = true
+
+    let state;
+
+    //number => 2 to power 53
+    //bigint
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefine =>
+    //symbol => unique
+
+
+    //object
+
+    console.log(typeof undefined);//undefined
+    console.log(typeof null)//object
+
