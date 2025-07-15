0059ab2b8117c17df1342a37087eac0e313ffdc1
sachin7091 <sachinkumar37958@gmail.com>
2025-07-16 01:02:49 +0530

    Camparision

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,24 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+                                  
+// these conversion must be avoid because here null convert any of thing like as a "0" , Nan | ye sab comparision confusion me dal dete hai
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === three assin operator value ke sath data type v dekhta hai, same ke case me true warna false deta h
+
+console.log("2" === 2 ) ;
