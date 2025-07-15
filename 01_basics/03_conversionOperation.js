9de5126ca95450181f8e67e06068081b1d61fa9b
sachin7091 <sachinkumar37958@gmail.com>
2025-07-16 00:43:13 +0530

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,27 +1,66 @@
-let score = "sachin"
+// let score = "sachin"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
-//"33" => 33
-//"33abc" => Nan (not a number)
-//true => 1; false => 0
+// //"33" => 33
+// //"33abc" => Nan (not a number)
+// //true => 1; false => 0
 
-let isLoggedIn = "sachin"
+// let isLoggedIn = "sachin"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn)
 
-//1 => true; 0 => false 
-//"" => false
-//"sachin" => true
+// //1 => true; 0 => false 
+// //"" => false
+// //"sachin" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// *******************Operations******************
+
+let value = 3
+let negval = -value;
+// console.log(negval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2/3);
+
+// let str1 = "sachin";
+// let str2 = " kumar";
+// let str3 = str1+str2;
+// console.log(str3);
+
+// console.log(1 + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+// console.log(true+);  //wrong not exist
+console.log(+"");
+
+let num1, num2, num3; 
+num1 = num2 = num3 = 2 +2;// bad assignment manner
+console.table([num1,num2,num3]);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
